fede264db68a6b4c7053cabe401a645243a2d98b
Jorge989 <attiejorge89@gmail.com>
2021-07-11 13:07:49 -0300

    ajustando heroku

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 const path = require("path");
+const PORT = process.env.PORT || 3333;
+const HOST = `http://localhost:${PORT}`;
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@cluster0.yv5la.mongodb.net/semana09?retryWrites=true&w=majority",
   {
@@ -15,4 +17,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
-app.listen(3333, console.log("🔥 Servidor rodando!"));
+// const server = https.createServer(app);
+app.listen(PORT, () => console.log("🔥 Servidor rodando!"));
+// app.listen(3333, console.log("🔥 Servidor rodando!"));
