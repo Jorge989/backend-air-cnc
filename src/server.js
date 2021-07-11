5c644a905359a974b00f8760c0be81618de54501
Jorge989 <attiejorge89@gmail.com>
2021-07-11 17:02:18 -0300

    go

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const routes = require("./routes");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT || 5000;
 const path = require("path");
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@cluster0.yv5la.mongodb.net/semana09?retryWrites=true&w=majority",
@@ -15,4 +16,4 @@ app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
-app.listen(3333, console.log("🔥 Servidor rodando!"));
+app.listen(PORT, console.log("🔥 Servidor rodando!"));
