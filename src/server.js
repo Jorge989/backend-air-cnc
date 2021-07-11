70600952a2750b776c7faedb354539d849be0c21
Jorge989 <attiejorge89@gmail.com>
2021-07-11 16:11:44 -0300

    for

--- src/server.js
+++ src/server.js
@@ -2,11 +2,8 @@ const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const http = require("https");
 const app = express();
 const path = require("path");
-const PORT = process.env.PORT || 3333;
-const HOST = `https://localhost:${PORT}`;
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@cluster0.yv5la.mongodb.net/semana09?retryWrites=true&w=majority",
   {
@@ -16,9 +13,6 @@ mongoose.connect(
 );
 app.use(cors());
 app.use(express.json());
-console.log("hello");
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
-const server = http.createServer(app);
-server.listen(PORT, () => console.log(`🔥 Servidor rodando!${HOST}`));
-// app.listen(3333, console.log("🔥 Servidor rodando!"));
+app.listen(3333, console.log("🔥 Servidor rodando!"));
